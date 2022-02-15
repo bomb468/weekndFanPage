2ca0f3a72058b1c142f430c35612333e00bb84b1
Gurpreet Singh <72384743+bomb468@users.noreply.github.com>
2022-02-15 14:35:43 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -287,10 +287,12 @@ const PlaylistSongsindexes=[[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,2
 [30,31,32,33,34,35],
 [36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53]];
 let List
-if (localStorage.customIndexes === undefined) {
+if (!localStorage.customIndexes) {
     //localStorage.customIndexes = JSON.stringify([]);
     List=[];
+    console.log('reached');
 }else{
+    console.log('not reached');
     List=JSON.parse(localStorage.customIndexes);
 }
 PlaylistSongsindexes.push(List);
